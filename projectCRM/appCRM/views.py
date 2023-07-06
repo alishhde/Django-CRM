4679e67ccb827595f9158e5e898cd341ae53c404
Ali Shohadaee <al.shohadaee@gmail.com>
2023-07-06 12:06:52 +0330

    login done

--- projectCRM/appCRM/views.py
+++ projectCRM/appCRM/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    context = {
+
+    }
+
+    # Check to see if user is logging in (on the other words, if user is posting data)
+    if request.method == 'POST':
+        username = request.POST['username']  # The 'username' is what we have defined in our form in home.html
+        password = request.POST['password']
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in.")
+            return redirect('home')
+        else:
+            messages.success(request, "Invalid username or password")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', context)
+
+
+def logout_user(request):
+    context = {
+
+    }
